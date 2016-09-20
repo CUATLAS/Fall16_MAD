56b761ddf12fffe8a08ce5efdbb4a9734ade7315
Aileen J Pierce <aileen.pierce@colorado.edu>
2016-09-20 12:35:35 -0600

    tip calc

---
+++ week5/tipcalculator/tipcalculator/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Aileen Pierce
+//  Copyright (c) 2016 Aileen Pierce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var checkAmount: UITextField!
+    @IBOutlet weak var tipPercent: UITextField!
+    @IBOutlet weak var people: UITextField!
+    @IBOutlet weak var tipDue: UILabel!
+    @IBOutlet weak var totalDue: UILabel!
+    @IBOutlet weak var totalDuePerPerson: UILabel!
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func updateTipTotals() {
+        var amount:Float //check amount
+        var pct:Float //tip percentage
+        
+        if checkAmount.text!.isEmpty {
+           amount = 0.0
+        } else {
+            amount = Float(checkAmount.text!)!
+        }
+        if tipPercent.text!.isEmpty {
+            pct = 0.0
+        }
+        else {
+            pct = Float(tipPercent.text!)!/100
+        }
+
+        let numberOfPeople=Int(people.text!) //returns an optional
+        let tip=amount*pct
+        let total=amount+tip
+        var personTotal : Float = 0.0 //specify Float so it's not a Double
+        if numberOfPeople != nil {
+            if numberOfPeople! > 0 {
+                personTotal = total / Float(numberOfPeople!)
+            }
+        }
+        
+        //format results as currency
+        let currencyFormatter = NSNumberFormatter()
+        currencyFormatter.numberStyle=NSNumberFormatterStyle.CurrencyStyle //set the number style
+        tipDue.text=currencyFormatter.stringFromNumber(tip) //returns a formatted string
+        totalDue.text=currencyFormatter.stringFromNumber(total)
+        totalDuePerPerson.text=currencyFormatter.stringFromNumber(personTotal)
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        updateTipTotals()
+    }
+    
+    override func viewDidLoad() {
+        checkAmount.delegate=self
+        tipPercent.delegate=self
+        people.delegate=self
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
